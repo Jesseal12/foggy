e2b2f9bd197f1f07b5095f51e71d7f2544efe45a
AriPunavuori <38551509+AriPunavuori@users.noreply.github.com>
2025-11-02 10:24:48 +0200

    Smoothing to movement

--- Assets/Scripts/NPCMover.cs
+++ Assets/Scripts/NPCMover.cs
@@ -5,8 +5,11 @@ public class NPCMover : MonoBehaviour
     private Vector2 _targetOne = Vector2.right * 4.5f;
     private Vector2 _targetTwo = Vector2.right * -4.5f;
     private Vector2 _target;
+    
     private const float ProximityThreshold = 0.1f;
-    private const float Speed = 1f;
+    private const float MaxSpeed = 2f;
+    private const float MinSpeedFactor = .1f;
+    private const float MaxSpeedFactor = 1f;
     
     private void Start()
     {
@@ -15,7 +18,7 @@ public class NPCMover : MonoBehaviour
     
     private void FixedUpdate()
     {
-        if (ProximityCheck())
+        if (TimeToReturn())
         {
             ChangeTarget();
             FlipCharacter();
@@ -23,17 +26,24 @@ public class NPCMover : MonoBehaviour
         
         Move();
     }
-
+    
     private void Move()
     {
-        transform.position = Vector2.MoveTowards(transform.position, _target, Speed * Time.fixedDeltaTime);
-        //_rb2d.MovePosition(_rb2d.position + _velocity * Time.fixedDeltaTime);
+        float smoothSpeed = Mathf.Clamp(ProximityCheck(), MinSpeedFactor, MaxSpeedFactor) * MaxSpeed;
+        transform.position = Vector2.MoveTowards(transform.position, _target, smoothSpeed * Time.fixedDeltaTime);
     }
-    private bool ProximityCheck()
+    private float ProximityCheck()
     {
-        return Vector2.Distance(transform.position, _target) < ProximityThreshold;
+        var dist1 = Vector2.Distance(transform.position, _targetOne);
+        var dist2 = Vector2.Distance(transform.position, _targetTwo);
+        
+        return dist1 > dist2 ? dist2 : dist1;
     }
 
+    private bool TimeToReturn()
+    { 
+        return Vector2.Distance(transform.position, _target) < ProximityThreshold;
+    }
     private void ChangeTarget()
     {
         _target = _target == _targetOne ? _targetTwo : _targetOne;
